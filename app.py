ea5bcbf0e5482a988f1d9a41e320e7dfca7bc1da
heyitssatyam <kumarsatyam2003@gmail.com>
2024-07-03 22:36:55 +0530

    Fix Errors

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ from flask_jwt_extended import (
 )
 from helpers import serialize_mongo_doc
 from bson.objectid import ObjectId
-from datetime import timedelta
+from datetime import timedelta, datetime
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
 app.config["MONGO_URI"] = MONGODB_URI
 app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
@@ -30,6 +33,7 @@ def home():
 
 @app.route("/login", methods=["POST"])
 def login():
+    print(request.form)
     email = request.form["email"]
     password = request.form["password"]
     password_hash = sha256(password.encode("utf-8")).hexdigest()
@@ -39,7 +43,13 @@ def login():
     )
     if doc is not None:
         access_key = create_access_token(identity=doc["username"])
-        return {"message": "Successful login", "access_token": access_key}
+        curDate = datetime.now()
+        expDate = curDate + timedelta(days=1)
+        return {
+            "message": "Successful login",
+            "access_token": access_key,
+            "expires_at": datetime.isoformat(expDate),
+        }
     else:
         return {"message": "Login failed"}, 401
 
@@ -101,6 +111,13 @@ def singleMember(member_id):
     elif request.method == "DELETE":
         mongo.db.get_collection("members").delete_one({"_id": ObjectId(member_id)})
         return {"members": "Member deleted"}
+
+
+@app.route("/trainers", methods=["GET", "POST"])
+@jwt_required
+def trainers():
+    if request.method("GET"):
+        trainers= mongo.db.get_collection("trainers").find()
 
 
 if __name__ == "__main__":
